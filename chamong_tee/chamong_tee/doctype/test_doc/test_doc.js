3ff76d49201ed054962b043fb3519b3adc463333
beetashoke <beetashoke.chakraborty@clapgrow.com>
2025-08-02 17:33:23 +0530

    Initial commit of chamong_tee app

---
+++ chamong_tee/chamong_tee/doctype/test_doc/test_doc.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, beetashoke chakraborty and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Test Doc", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ chamong_tee/chamong_tee/doctype/test_doc/test_test_doc.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, beetashoke chakraborty and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTestDoc(FrappeTestCase):
+	pass
